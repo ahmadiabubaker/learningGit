b8ef06fddea30fc2d7723504acfd59191893963a
abubakerahmadi <abubakerahmadi45@gmail.com>
2025-04-06 16:07:26 -0400

    new changes 4/6

--- testjava.java
+++ testjava.java
@@ -1,6 +1,9 @@
 public class testjava{
     public static void main(String[] args){
         System.out.println("test");
+        if(5<10){
+            System.out.println("Test was good");
+        }
     }
 
 }
